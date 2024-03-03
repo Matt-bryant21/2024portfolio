2f6a4aeb4fe12000398de4e04ca5b2aeadb61987
Matt-bryant21 <14771@coderacademy.edu.au>
2024-03-03 17:00:46 +1100

    app layout

---
+++ Portfolio/src/components/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch, BrowserRouter } from "react-router-dom";
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <Route />
+    </BrowserRouter>
+  )
+}
+
+export default App
