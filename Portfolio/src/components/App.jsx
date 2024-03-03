325d15ff538087b623c8ee49cb7d80e1b8a2bf35
Matt-bryant21 <14771@coderacademy.edu.au>
2024-03-03 17:11:44 +1100

    app layout and routed links

--- Portfolio/src/components/App.jsx
+++ Portfolio/src/components/App.jsx
@@ -1,16 +1,22 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route, Switch, BrowserRouter } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./Homepage";
+import AboutMe from "./AboutMe";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 function App() {
-  
-
   return (
-    <BrowserRouter>
-      <Route />
-    </BrowserRouter>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ Portfolio/src/components/Homepage.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Homepage = () => {
+  return <h1>Homepage</h1>;
+};
+
+export default Homepage;
