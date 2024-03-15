012c698ee4b0f684efc6ee31212259024ebff098
Matt-bryant21 <14771@coderacademy.edu.au>
2024-03-15 16:19:35 +1100

    fixed footer font size

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -4,17 +4,20 @@ const Footer = () => {
   return (
     <>
       <nav className="grid grid-cols-2 gap-4 pt-2">
-        <div className="text-backgroundgrey text-left pl-10 hover:text-green py-4">
+        <div className="text-backgroundgrey text-sm text-left pl-10 hover:text-green py-4">
           <a href="/aboutme">Designed by Matt Bryant 2024 &trade;</a>
         </div>
         <div className="flex justify-end gap-16 px-20 py-4">
-          <a href="www.uncelebratedjewellery.com" className="text-backgroundgrey hover:text-green">
+          <a href="www.uncelebratedjewellery.com" className="text-backgroundgrey text-sm hover:text-green">
             uncelebrated jewellery
           </a>
-          <a href="https://www.linkedin.com/in/matt-bryant-5813a2293/" className="text-backgroundgrey hover:text-green">
+          <a
+            href="https://www.linkedin.com/in/matt-bryant-5813a2293/"
+            className="text-backgroundgrey text-sm hover:text-green"
+          >
             linkedin
           </a>
-          <a href="https://github.com/Matt-bryant21" className="text-backgroundgrey hover:text-green">
+          <a href="https://github.com/Matt-bryant21" className="text-backgroundgrey text-sm hover:text-green">
             github
           </a>
         </div>
--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
     <>
       <nav className="grid grid-cols-3 gap-4 items-center pt-2 sticky top-0 z-10 bg-backgrounddark">
-        <a href="/aboutme" className="text-backgroundgrey text-left pl-10 hover:text-blue">
+        <a href="/aboutme" className="text-backgroundgrey text-left pl-10 hover:text-blue text-sm">
           about me &#x263A;
         </a>
-        <a href="/projects" className="text-backgroundgrey text-center hover:text-blue">
+        <a href="/projects" className="text-backgroundgrey text-sm text-center hover:text-blue">
           projects &#x263A;
         </a>
-        <a href="/contact" className="text-backgroundgrey text-right pr-10 hover:text-blue">
+        <a href="/contact" className="text-backgroundgrey text-sm text-right pr-10 hover:text-blue">
           contact &#x263A;
         </a>
       </nav>
