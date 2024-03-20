2797ac1131ff89f7f6046d851b93753e5471eb3d
Matt-bryant21 <14771@coderacademy.edu.au>
2024-03-20 16:57:14 +1100

    scrapped original layout

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -1,29 +1,29 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <nav className="grid grid-cols-2 gap-4 pt-2">
-        <div className="text-backgroundgrey text-sm text-left pl-10 hover:text-green py-4">
-          <a href="/aboutme">Designed by Matt Bryant 2024 &trade;</a>
-        </div>
-        <div className="flex justify-end gap-16 px-20 py-4">
-          <a href="www.uncelebratedjewellery.com" className="text-backgroundgrey text-sm hover:text-green">
-            uncelebrated jewellery
-          </a>
-          <a
-            href="https://www.linkedin.com/in/matt-bryant-5813a2293/"
-            className="text-backgroundgrey text-sm hover:text-green"
-          >
-            linkedin
-          </a>
-          <a href="https://github.com/Matt-bryant21" className="text-backgroundgrey text-sm hover:text-green">
-            github
-          </a>
-        </div>
-      </nav>
-    </>
+    <nav className="flex flex-col lg:flex-row justify-between items-center py-4 lg:py-2 ">
+      <div className="text-backgroundgrey text-sm text-center lg:text-left lg:pl-10 hover:text-green lg:py-0">
+        <a href="/aboutme">Designed by Matt Bryant 2024 &trade;</a>
+      </div>
+      <div className="flex justify-center lg:justify-end gap-4 lg:gap-16 px-4 lg:px-20 py-2 lg:py-0">
+        <a href="https://www.uncelebratedjewellery.com" className="text-backgroundgrey text-sm hover:text-green">
+          uncelebrated jewellery
+        </a>
+        <a
+          href="https://www.linkedin.com/in/matt-bryant-5813a2293/"
+          className="text-backgroundgrey text-sm hover:text-green"
+        >
+          linkedin
+        </a>
+        <a href="https://github.com/Matt-bryant21" className="text-backgroundgrey text-sm hover:text-green">
+          github
+        </a>
+      </div>
+    </nav>
   );
 };
 
 export default Footer;
+
--- Portfolio/src/components/Homepage.jsx
+++ Portfolio/src/components/Homepage.jsx
@@ -1,27 +1,28 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 const Homepage = () => {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <div className="flex justify-center w-full relative">
-          <div className="flex flex-col items-center">
-            <h1 className="text-center">
+      <div className="flex xl:flex-col relative">
+        <div className="flex justify-center xl:w-2/4 xl:z-10 xl:relative">
+          <div className="flex flex-col items-center xl:ml-64 xl:mt-8">
+            <h1>
               HEY, I&apos;M <span className="text-backgroundgrey">MATT</span>
             </h1>
-            <h1 className="text-center">
+            <h1>
               HEY, I&apos;M <span className="text-backgroundgrey">MATT</span>
             </h1>
-            <h1 className="text-center">
+            <h1>
               HEY, I&apos;M <span className="text-backgrounddark">MATT</span>
             </h1>
-            <h1 className="text-center">
+            <h1>
               HEY, I&apos;M <span className="text-backgrounddark">MATT</span>
             </h1>
           </div>
         </div>
-        <div className="flex justify-center bg-backgroundgrey mt-4 xl:w-full xl:ml-auto xl:mr-0 xl:mb-4 xl:-mt-16 xl:h-auto">
-          <div className="flex flex-col items-center px-4 py-3">
+        <div className="flex justify-end bg-backgroundgrey xl:ml-auto xl:mr-80 xl:mb-16 xl:-mt-64 xl:h-1/3">
+          <div className="flex xl:flex-col xl:items-end px-32 py-3">
             <h2>JUNIOR</h2>
             <h2>FULL STACK</h2>
             <h2>WEB DEVELOPER</h2>
