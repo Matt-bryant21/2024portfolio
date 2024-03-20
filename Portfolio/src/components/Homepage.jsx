b14efec410d23ffd54a757aa8040e389039fc014
Matt-bryant21 <14771@coderacademy.edu.au>
2024-03-20 16:25:56 +1100

    changed layout for responsiveness :(

--- Portfolio/src/components/Homepage.jsx
+++ Portfolio/src/components/Homepage.jsx
@@ -1,33 +1,27 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 const Homepage = () => {
   return (
     <>
-      {/* main div */}
-      <div className="flex flex-col relative">
-        {" "}
-        {/* Added relative positioning */}
-        <div className="flex justify-center w-2/4 z-10 relative">
-          {" "}
-          {/* Added z-index and relative positioning */}
-          <div className="flex flex-col items-center ml-56 mt-8">
-            <h1>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center w-full relative">
+          <div className="flex flex-col items-center">
+            <h1 className="text-center">
               HEY, I&apos;M <span className="text-backgroundgrey">MATT</span>
             </h1>
-            <h1>
+            <h1 className="text-center">
               HEY, I&apos;M <span className="text-backgroundgrey">MATT</span>
             </h1>
-            <h1>
+            <h1 className="text-center">
               HEY, I&apos;M <span className="text-backgrounddark">MATT</span>
             </h1>
-            <h1>
+            <h1 className="text-center">
               HEY, I&apos;M <span className="text-backgrounddark">MATT</span>
             </h1>
           </div>
         </div>
-        <div className="flex justify-end bg-backgroundgrey ml-auto mr-80 mb-16 -mt-64 h-1/3">
-          <div className="flex flex-col items-end px-32 py-3">
+        <div className="flex justify-center bg-backgroundgrey mt-4 xl:w-full xl:ml-auto xl:mr-0 xl:mb-4 xl:-mt-16 xl:h-auto">
+          <div className="flex flex-col items-center px-4 py-3">
             <h2>JUNIOR</h2>
             <h2>FULL STACK</h2>
             <h2>WEB DEVELOPER</h2>
