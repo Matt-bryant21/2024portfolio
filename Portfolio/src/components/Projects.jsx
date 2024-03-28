5104bc533d2782b9d08cce1dc3d675d606c5d634
Matt-bryant21 <14771@coderacademy.edu.au>
2024-03-28 22:36:53 +1100

    changed project cards

--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
         <div>
           <h2 className="text-4xl text-center my-8">PROJECTS PROJECTS PROJECTS</h2>
         </div>
-        <div className="flex flex-col items-center justify-start w-full sm:w-3/4 lg:w-2/4 mx-auto bg-backgroundgrey border border-blue rounded-lg p-5 shadow-md h-96 overflow-y-auto">
+        <div className="flex flex-col items-start justify-start w-full bg-backgroundgrey border border-grey rounded-lg p-5 shadow-md h-96 overflow-y-auto">
           <h2 className="text-5xl uppercase text-backgrounddark font-bold mb-2">Project Title</h2>
           <p className="text-xl">
             <a href="http://your-project-url.com">Github Link</a>
