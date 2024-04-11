a7114333ae154faf61126b94e0923b00d6c39e1e
Matt-bryant21 <14771@coderacademy.edu.au>
2024-04-11 10:32:24 +1000

    fixed navbar links

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,27 +1,25 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <nav className="flex sm:justify-center xl:justify-between items-center py-2 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 sticky top-0 z-10 bg-backgrounddark">
-        <div className="flex justify-center xl:justify-between items-center flex-grow">
-          <a href="/" className="text-backgroundgrey hover:text-blue text-sm">
-            home &#x263A;&#xFE0E;
-          </a>
-          <a href="/aboutme" className="text-backgroundgrey hover:text-blue text-sm ml-6">
-            about me &#x263A;&#xFE0E;
-          </a>
-          <a href="/projects" className="text-backgroundgrey hover:text-blue text-sm ml-6">
-            projects &#x263A;&#xFE0E;
-          </a>
-          <a href="/contact" className="text-backgroundgrey hover:text-blue text-sm ml-6">
-            contact &#x263A;&#xFE0E;
-          </a>
-        </div>
-        <div className="flex-shrink-0">{/* Add any additional content here */}</div>
-      </nav>
-    </>
+    <nav className="flex sm:justify-center xl:justify-between items-center py-2 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 sticky top-0 z-10 bg-backgrounddark">
+      <div className="flex justify-center xl:justify-between items-center flex-grow">
+        <Link to="/" className="text-backgroundgrey hover:text-blue text-sm">
+          home &#x263A;&#xFE0E;
+        </Link>
+        <Link to="/aboutme" className="text-backgroundgrey hover:text-blue text-sm ml-6">
+          about me &#x263A;&#xFE0E;
+        </Link>
+        <Link to="/projects" className="text-backgroundgrey hover:text-blue text-sm ml-6">
+          projects &#x263A;&#xFE0E;
+        </Link>
+        <Link to="/contact" className="text-backgroundgrey hover:text-blue text-sm ml-6">
+          contact &#x263A;&#xFE0E;
+        </Link>
+      </div>
+      <div className="flex-shrink-0">{/* Add any additional content here */}</div>
+    </nav>
   );
 };
 
